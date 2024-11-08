5fb028ed78ba135944bf6617ffebe32c9d72cc7b
Code addict <jonalgaming@gmail.com>
2024-11-08 16:37:02 +0100

    added recurisve group members, updated readme, some color coding for easier readability and updated the commands to make more sense#

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # About
 AD recon / exploits tools that I have made to play around with LDAP and AD. 
+
+# TODO
+- [ ] When looking at an object, it will recursivly un-nest any nested groups. However, it only does it one way, meaning it can do: 
+
+    ```
+    .\recon.ps1 fg "Management *"
+        ...
+        Is a member of:
+        - Development Department
+            - Management Department
+                - jen
+        Members are:
+        - jen
+    ``` 
+    Here we can clearly see that jen is not just a memeber of managment, but is also a member of dev. Meaning jen has both the permissiosn of managment, but also dev, very nice!
+
+    However, if we search for jen as a user
+    ```
+    .\recon.ps1 fu "jen"
+        ...
+        name: jen
+        Is a member of:
+        - Management Department
+        ...
+    ```
+    We would miss the fact that she is a memeber of dev.
+
+- [ ] Add ACL tracking. I dont care what sub-groups exists under an account, if we can't do anything with those groups.
--- recon.ps1
+++ recon.ps1
@@ -1,16 +1,34 @@
+#Color scheme idea:
+# Red = pay attantion here (like "Found a domain admin")
+# Green = interesting information (like "none-standard group found")
+# Cyan = focus here (like, here is where group and samaccount data is)
+# Yellow = sub-focus. So if we use Cyan to highlight a headline (like "Memebers are") we can use Yellow to highlight the memeber's names.
+
 param (
     [string]$FunctionKeyword,
     [string]$Parameter1        
 )
 
 
-# Define a list of standard AD groups from the provided Microsoft documentation
+# Define a list of standard AD groups that from the provided Microsoft documentation. 
 $standardGroups = @(
     "Account Operators", "Administrators", "Backup Operators", "Domain Admins", "Domain Guests",
     "Domain Users", "Enterprise Admins", "Group Policy Creator Owners", "Guests", "Incoming Forest Trust Builders",
     "Network Configuration Operators", "Performance Log Users", "Performance Monitor Users", "Pre-Windows 2000 Compatible Access",
     "Print Operators", "Remote Desktop Users", "Replicator", "Schema Admins", "Server Operators", "Users"
 )
+
+# A list of high value ad groups
+$highValueGroups = @(
+    "Administrators",
+    "Domain Admins",
+    "Enterprise Admins",
+    "Schema Admins",
+    "Account Operators",
+    "Backup Operators",
+    "Server Operators"
+)
+
 
 # Returns the current primary domain controller (PDC) which is the domain controller that we want to query to make sure all data is up-to-date.
 function Get-PrimaryDomainController {
@@ -49,6 +67,86 @@ function Get-PropertiesFromObjectsOld($objects) {
     }
 }
 
+# Checks if an object is a group. Returns True if it is, False if it not.
+function Test-IfObjectIsAGroup($objectName){
+    $filter = "(&(objectCategory=group)(cn=$objectName))"
+    $object = Find-ObjectBasedOnFilter -baseLdapQuery $baseLdapQuery -filter $filter
+    if ($object.Count -gt 0) {
+        return $true
+    }
+    else {
+        return $false
+    }
+}
+
+# Returns a list of members from an object
+function Get-MembersFromObject($object) {
+    $members = @()    
+    if ($object -and $object.Properties["member"]) {
+        foreach ($member in $object.Properties["member"]) {
+            $members += $member
+        }
+    }
+    
+    return $members
+}
+
+# Prints all groups inside another group recursivly, in a user friendly way.
+function Get-AllGroupsRecursively ($groupName, $level) {
+    $groupObject = Find-Group -baseLdapQuery $baseLdapQuery -groupName $groupName
+    
+    if ($groupObject.Count -gt 0) {
+        $indentation = " " * ($level * 4)
+        
+        if ($highValueGroups -contains $groupName) {
+            Write-Host "$indentation- $groupName" -ForegroundColor Red
+        }
+        elseif ($standardGroups -contains $groupName) {
+            Write-Host "$indentation- $groupName"
+        }
+        else {
+            Write-Host "$indentation- $groupName" -ForegroundColor Green
+        }
+
+        foreach ($member in $groupObject[0].Properties["member"]) {
+            $memberName = $member -replace '^CN=([^,]+),.*', '$1'
+            if (Test-IfObjectIsAGroup $memberName) {
+                Get-AllGroupMembersRecursively -groupName $memberName -level ($level + 1)
+            }
+        }
+    }
+}
+
+
+# Prints all groups and users of each group recursivly, in a user friendly way.
+function Get-AllGroupMembersRecursively ($groupName, $level) {
+    $groupObject = Find-Group -baseLdapQuery $baseLdapQuery -groupName $groupName
+
+    if ($groupObject.Count -gt 0) {
+        $indentation = " " * ($level * 4)
+        
+        if ($highValueGroups -contains $groupName) {
+            Write-Host "$indentation- $groupName" -ForegroundColor Red
+        }
+        elseif ($standardGroups -contains $groupName) {
+            Write-Host "$indentation- $groupName"
+        }
+        else {
+            Write-Host "$indentation- $groupName" -ForegroundColor Green
+        }
+
+        foreach ($member in $groupObject[0].Properties["member"]) {
+            $memberName = $member -replace '^CN=([^,]+),.*', '$1'
+            if (Test-IfObjectIsAGroup $memberName) {
+                Get-AllGroupMembersRecursively -groupName $memberName -level ($level + 1)
+            }
+            else {
+                Write-Host "$indentation    - $memberName" -ForegroundColor Yellow  # Print member with indentation
+            }
+        }
+    }
+}
+
 # Prints all properties in an object but adds coloring for clarity
 function Get-PropertiesFromObjectsPrettyPrint($objects) {
     Foreach($obj in $objects) {
@@ -64,17 +162,29 @@ function Get-PropertiesFromObjectsPrettyPrint($objects) {
                 Write-Host "$propName $propValue" -ForegroundColor Cyan
                 Continue
             }
-            
-            if ($propName -eq "memberof") {
-                Write-Host "Is a memeber of:" -ForegroundColor Cyan
-                Foreach ($group in $obj.Properties["memberof"]) {
-                    $groupName = $group -replace '^CN=([^,]+),.*', '$1'
-                    
-                    if ($standardGroups -contains $groupName) {
-                        Write-Host " - $groupName"
-                    } else {
-                        Write-Host " - $groupName" -ForegroundColor Red
+
+            # What members are a part of a given object
+            if ($propName -eq "member" ) {
+                Write-Host "Members are:" -ForegroundColor Cyan
+                Foreach ($member in $obj.Properties["member"]){
+                    $memberName = $member -replace '^CN=([^,]+),.*', '$1'
+                    if (Test-IfObjectIsAGroup $memberName){
+                        Get-AllGroupMembersRecursively -groupName $memberName -level 0
                     }
+                    else{
+                        Write-Host " - $memberName" -ForegroundColor Yellow
+                    }
+
+                }
+            }
+            
+            # What groups the object is a member of
+            # TODO: un-nest groups to see all groups the memeber is a part of.
+            if ($propName -eq "memberof") { 
+                Write-Host "Is a member of:" -ForegroundColor Cyan
+                foreach ($group in $obj.Properties["memberof"]) {
+                    $groupName = $group -replace '^CN=([^,]+),.*', '$1'
+                    Get-AllGroupsRecursively -groupName $groupName -level 0
                 }
             }
 
@@ -102,6 +212,17 @@ function Find-AllUsersAccounts($baseLdapQuery) {
 # Finds all "users"
 function Find-AllUsers($baseLdapQuery) { 
     $filter = "(&(objectCategory=person)(objectClass=user))"
+    return Find-ObjectBasedOnFilter $baseLdapQuery $filter
+}
+
+# Finds all groups. This will find more groups than the usual "net.exe" command. Because we enumerate all AD objects including Domain Local groups. Not just global groups.
+function Find-AllGroups ($baseLdapQuery){
+    $filter = "(objectclass=group)"
+    return Find-ObjectBasedOnFilter $baseLdapQuery $filter
+}
+
+function Find-Group ($baseLdapQuery, $groupName){
+    $filter = "(&(objectCategory=group)(cn=$groupName))"
     return Find-ObjectBasedOnFilter $baseLdapQuery $filter
 }
 
@@ -144,14 +265,24 @@ if ($FunctionKeyword -eq "-h" -or $FunctionKeyword -eq "--help") {
 
 # Switch to handle function calls based on keywords
 switch ($FunctionKeyword.ToLower()) {
-    "fu" {  # Short for find user
+    "fu" {  # Short for Find-User
         $result = Find-UserBasedOnName -baseLdapQuery $baseLdapQuery -name $Parameter1
         Get-PropertiesFromObjectsPrettyPrint $result  
     }
-    "fa" {  # Short for Find-AllUsers
+    "fau" {  # Short for Find-AllUsers
         $result = Find-AllUsers -baseLdapQuery $baseLdapQuery
         Get-PropertiesFromObjectsPrettyPrint $result  
     }
+    "fag"{ # Short for Find-AllGroups
+        $result = Find-AllGroups -baseLdapQuery $baseLdapQuery
+        Get-PropertiesFromObjectsPrettyPrint $result
+    }
+
+    "fg"{ # Short for Find-Group
+        $result = Find-Group -baseLdapQuery $baseLdapQuery -groupName $Parameter1
+        Get-PropertiesFromObjectsPrettyPrint $result
+    }
+
     "search"{
         $result = Find-ObjectBasedOnFilter -baseLdapQuery $baseLdapQuery -filter $Parameter1
         Get-PropertiesFromObjectsPrettyPrint $result
